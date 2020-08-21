85093c068a329931a55a177088ade95f8f3cf8ca
Shimmy Kurowski <35338718+shimmywhocares@users.noreply.github.com>
2020-08-21 10:46:17 +0200

    added ToolActivity basic layout ver 1.0

--- app/src/main/java/com/shimmy/toolsmanagement/ToolRecViewAdapter.java
+++ app/src/main/java/com/shimmy/toolsmanagement/ToolRecViewAdapter.java
@@ -18,20 +18,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static com.shimmy.toolsmanagement.ToolActivity.TOOL_ID_KEY;
 
 public class ToolRecViewAdapter extends RecyclerView.Adapter<ToolRecViewAdapter.ViewHolder>{
     private static final String TAG = "ToolRecViewAdapter";
 
-    private List<Tool> toolsList = new ArrayList<>();
+    private ArrayList<Tool> toolsArrayList = new ArrayList<>();
     private Context mContext;
 
     public ToolRecViewAdapter(Context mContext) {
         this.mContext = mContext;
     }
-
 
     @NonNull
     @Override
@@ -44,38 +40,35 @@ public class ToolRecViewAdapter extends RecyclerView.Adapter<ToolRecViewAdapter.
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called");
 
-        //here I think I could implement the Pares API
-        holder.txtToolName.setText(toolsList.get(position).getName());
-        holder.txtManufacturer.setText(toolsList.get(position).getManufacturer());
+        //here I think I need to implement Pares API
+        holder.txtToolName.setText(toolsArrayList.get(position).getName());
+        holder.txtManufacturer.setText(toolsArrayList.get(position).getManufacturer());
         Glide.with(mContext)
                 .asBitmap()
-                .load(toolsList.get(position).getImageUrl())
+                .load(toolsArrayList.get(position).getImageUrl())
                 .into(holder.imgTool);
 
         holder.parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: to be Intent or not to be intent?");
+                Log.d(TAG, "onClick: Intent czy nie intent");
                 //Toast.makeText(mContext, toolsArrayList.get(position).getName() + " selected, yo!", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent (mContext, ToolActivity.class);
-                intent.putExtra(TOOL_ID_KEY, toolsList.get(position).getId());
-                //intent.putExtra("toolName", toolsList.get(position).getName());
-
                 mContext.startActivity(intent);
 
 
             }
         });
 
-        holder.txtStatus.setText(toolsList.get(position).getStatus());
+        holder.txtStatus.setText(toolsArrayList.get(position).getStatus());
         //holder.txtPlaceOfStaying.setText(toolsArrayList.get(position).getPlaceOfStaying());
-        holder.txtHolder.setText(toolsList.get(position).getHolder());
-        //String.valueOf for convert a int/float
-        holder.txtHoursOfUsage.setText(String.valueOf(toolsList.get(position).getHoursOfUsage()));
-        holder.txtToolID.setText(String.valueOf(toolsList.get(position).getId()));
+        holder.txtHolder.setText(toolsArrayList.get(position).getHolder());
+        //TODO: toString() somehow
+        holder.txtHoursOfUsage.setText(String.valueOf(toolsArrayList.get(position).getHoursOfUsage()));
+        holder.txtToolID.setText(String.valueOf(toolsArrayList.get(position).getId()));
 
         //turn on and off the visibility of expendedRelLayout
-        if(toolsList.get(position).isExpanded()){
+        if(toolsArrayList.get(position).isExpanded()){
             //In next method I need to pass the root. In this case the root is CardView - parent.
             androidx.transition.TransitionManager.beginDelayedTransition(holder.parent);
             holder.expandedRelLayout.setVisibility(View.VISIBLE);
@@ -91,12 +84,12 @@ public class ToolRecViewAdapter extends RecyclerView.Adapter<ToolRecViewAdapter.
     //It returns the number of items or size of list in RecView
     @Override
     public int getItemCount() {
-        return toolsList.size();
+        return toolsArrayList.size();
     }
 
-    public void setToolsArrayList(List<Tool> toolList) {
-        this.toolsList = toolList;
-        //we're going to refresh the data in recycler view
+    public void setToolsArrayList(ArrayList<Tool> toolArrayList) {
+        this.toolsArrayList = toolArrayList;
+        //we're going to refresh the data in our recycler view
         notifyDataSetChanged();
     }
 
@@ -133,7 +126,7 @@ public class ToolRecViewAdapter extends RecyclerView.Adapter<ToolRecViewAdapter.
                 @Override
                 public void onClick(View view) {
                     //I'm going to get the current tool that I'm looking into; the second option to do that is by onBind method
-                     Tool tool = toolsList.get(getAdapterPosition());
+                     Tool tool = toolsArrayList.get(getAdapterPosition());
 
                      //after getting the tool I'm going to change its field e.g isExpended; shorter ver of the negation
                      tool.setExpanded(!tool.isExpanded());
@@ -148,7 +141,7 @@ public class ToolRecViewAdapter extends RecyclerView.Adapter<ToolRecViewAdapter.
                 @Override
                 public void onClick(View view) {
                     //I'm going to get the current tool that I'm looking into; the second option is by onBind method
-                    Tool tool = toolsList.get(getAdapterPosition());
+                    Tool tool = toolsArrayList.get(getAdapterPosition());
 
                     //after getting the tool I'm going to change its field e.g isExpended; shorter ver of the negation
                     tool.setExpanded(!tool.isExpanded());
